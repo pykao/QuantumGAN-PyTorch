124f3cf26166ddc76bce8e51cebc973ba643d53e
Ken Kao <ken@insilicotw01.localdomain>
2021-10-26 15:37:39 +0800

    different training strategies

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     config = get_GAN_config()
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"]="1,6"
+    os.environ["CUDA_VISIBLE_DEVICES"]="4"
 
     # molecule dataset dir
     config.mol_data_dir = r'data/gdb9_9nodes.sparsedataset'
@@ -85,29 +85,29 @@ if __name__ == '__main__':
     # Quantum
     config.quantum = True
     config.qubits = 8
-    #config.qc_lr = 0.01
+    config.qc_lr = 0.01
 
 
     # Training
-    #config.mode = 'train'
-    #config.complexity = 'mr'
-    #config.batch_size = 128
-    #config.z_dim = 8
-    #config.num_epochs = 30
-    #config.n_critic = 5
+    config.mode = 'train'
+    config.complexity = 'mr'
+    config.batch_size = 128
+    config.z_dim = 8
+    config.num_epochs = 30
+    config.n_critic = 1
     # 1.0 for pure WGAN and 0.0 for pure RL
-    #config.lambda_wgan = 1
+    config.lambda_wgan = 1
 
     # Test
-    config.mode = "test"
-    config.test_epoch = 1
-    config.test_sample_size = 5000
-    config.z_dim = 8
-    config.complexity = 'mr'
+    #config.mode = "test"
+    #config.test_epoch = 30
+    #config.test_sample_size = 5000
+    #config.z_dim = 8
+    #config.complexity = 'mr'
     # MolGAN
     #config.saving_dir = r"results/GAN/20211014_103213/train"
     # Quantum
-    config.saving_dir = r"results/quantum-GAN/20211019_104707/train"
+    #config.saving_dir = r"results/quantum-GAN/20211019_104719/train"
 
 
 
