dc7352b78e532d2377f035e92e24566906cbf27b
Ken Kao <ken@insilicotw01.localdomain>
2021-12-07 14:20:02 +0800

    added more explanations;

--- main.py
+++ main.py
@@ -76,55 +76,64 @@ if __name__ == '__main__':
     config = get_GAN_config()
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
+
+    # Dataset
     # molecule dataset dir
     config.mol_data_dir = r'data/gdb9_9nodes.sparsedataset'
     #config.mol_data_dir = r'data/qm9_5k.sparsedataset'
 
+
     # Quantum
     # quantum circuit to generate inputs of MolGAN
     config.quantum = True
+    # number of qubit of quantum circuit
     config.qubits = 8
+    # number of layer of quantum circuit
+    config.layer = 3
     # update the parameters of quantum circuit
-    config.update_qc = False
+    config.update_qc = True
     # the learning rate of quantum circuit
     # None: same learning rate as g_lr
-    config.qc_lr = None
-    # number of layer for quantum circuit
-    config.layer = 1
+    config.qc_lr = 0.04
     # to use pretrained quantum circuit or not
-    config.qc_pretrained = True
+    # config.qc_pretrained = False
 
 
     # Training
     config.mode = 'train'
+    # the complexity of generator
     config.complexity = 'mr'
+    # batch size
     config.batch_size = 128
+    # input noise dimension
     config.z_dim = 8
-    config.num_epochs = 150
-    config.n_critic = 5
+    # number of epoch
+    config.num_epochs = 300
+    # n_critic
+    config.n_critic = 3
+    # critic type
     config.critic_type = 'D'
     # 1.0 for pure WGAN and 0.0 for pure RL
     config.lambda_wgan = 1
     # weight decay
-    #config.decay_every_epoch = None
-    #config.gamma = 0.1
+    config.decay_every_epoch = 60
+    config.gamma = 0.1
 
-    # Test
+
+    # Testing
     #config.mode = "test"
-    #config.test_epoch = 30
+    #config.complexity = 'mr'
     #config.test_sample_size = 5000
     #config.z_dim = 8
-    #config.complexity = 'mr'
+    #config.test_epoch = 30
     # MolGAN
     #config.saving_dir = r"results/GAN/20211014_151730/train"
     # Quantum
-    #config.saving_dir = r"results/quantum-GAN/20211122_120334/train"
+    #config.saving_dir = r"results/quantum-GAN/20211130_102404/train"
 
 
-
-    # Quantum
     if config.complexity == 'nr':
         config.g_conv_dim = [128, 256, 512]
     elif config.complexity == 'mr':
@@ -134,16 +143,18 @@ if __name__ == '__main__':
     else:
         raise ValueError("Please enter an valid model complexity from 'mr', 'hr' or 'nr'!")
 
+
+    # Quantum directory
     if config.quantum and config.mode == 'train':
         config.saving_dir = 'results/quantum-GAN'
 
+    # Quantum Circuit
     dev = qml.device('default.qubit', wires=config.qubits)
     @qml.qnode(dev, interface='torch', diff_method='backprop')
     def gen_circuit(w):
         # random noise as generator input
         z1 = random.uniform(-1, 1)
         z2 = random.uniform(-1, 1)
-
         # construct generator circuit for both atom vector and node matrix
         for i in range(config.qubits):
             qml.RY(np.arcsin(z1), wires=i)
